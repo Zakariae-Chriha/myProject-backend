fb49af353927d3afb175885913356d72cf7c5ca5
Martin <martin@flesk.es>
2022-05-05 15:39:01 +0200

    login repariert

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,61 +1,60 @@
-const bcrypt = require("bcrypt");
-const User = require("../modeles/userSchema");
-
-const jwt = require("jsonwebtoken");
+const bcrypt = require('bcrypt')
+const User = require('../modeles/userSchema')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
-  const { username, password, isAdmin } = req.body;
+  const { username, password, isAdmin } = req.body
 
-  const foundUser = await User.find({ username: username });
+  const foundUser = await User.find({ username: username })
 
   if (foundUser.length > 0) {
-    res.status(400).send("Username already exists");
+    res.status(400).send('Username already exists')
   } else {
-    const hashedPassword = await bcrypt.hash(password, 10);
+    const hashedPassword = await bcrypt.hash(password, 10)
 
     const user = await User.create({
       username: username,
       password: hashedPassword,
       isAdmin: isAdmin,
-    });
+    })
 
     const token = jwt.sign(
       { username: username, isAdmin: isAdmin },
       process.env.JWT_SECRET
-    );
+    )
 
-    res.status(200).json({ token });
+    res.status(200).json({ token })
   }
-};
+}
 
 const login = async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password } = req.body
 
-  const foundUser = await User.find({ username: username });
+  const foundUser = await User.find({ username: username })
 
-  if (foundUser.length == 0) {
-    res.status(400).send("Username does not exist");
+  if (foundUser.length < 1) {
+    res.status(400).send('Username does not exist')
   } else {
-    const match = await bcrypt.compare(password, foundUser[0].password);
+    const match = await bcrypt.compare(password, foundUser[0].password)
     if (!match) {
-      res.status(400).send("Password is incorrect");
+      res.status(400).send('Password is incorrect')
     } else {
       const token = jwt.sign(
-        { username: foundUser.username, isAdmin: foundUser.isAdmin },
+        { username: foundUser[0].username, isAdmin: foundUser[0].isAdmin },
         process.env.JWT_SECRET
-      );
+      )
 
-      res.status(200).json({ token });
+      res.status(200).json({ token })
     }
   }
-};
+}
 
 const approveSession = async (req, res) => {
-  res.json({ success: "valid token" });
-};
+  res.json({ success: 'valid token' })
+}
 
 const getUserInfo = async (req, res) => {
-  res.send(req.user);
-};
+  res.send(req.user)
+}
 
-module.exports = { login, register, getUserInfo, approveSession };
+module.exports = { login, register, getUserInfo, approveSession }
--- middlewares/verifyToken.js
+++ middlewares/verifyToken.js
@@ -1,27 +1,28 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken')
+const User = require('../modeles/userSchema')
 
 // middleware to check if user is logged in
 const verifyToken = async (req, res, next) => {
   try {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization
 
     if (!token) {
-      res.status(401).json({ error: "Not authorized" });
+      res.status(401).json({ error: 'Not authorized' })
     } else {
-      const decodedUser = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decodedUser;
+      const decodedUser = jwt.verify(token, process.env.JWT_SECRET)
+      req.user = decodedUser
 
-      const foundUser = await User.find({ username: decodedUser.username });
-
-      if (foundUser.length > 0) {
-        res.status(400).json({ error: "User does not exist" });
+      const foundUser = await User.find({ username: decodedUser.username })
+      console.log('foundUser', foundUser)
+      if (!foundUser.length) {
+        res.status(400).json({ error: 'User does not exist' })
+      } else {
+        next()
       }
-
-      next();
     }
   } catch (err) {
-    res.status(401).json({ error: "Invalid token" });
+    res.status(401).json({ error: 'Invalid token' })
   }
-};
+}
 
-module.exports = { verifyToken };
+module.exports = { verifyToken }
