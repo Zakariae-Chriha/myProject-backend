9ba7e8928d65f6f9ddc23846e336a623fea6b88e
zakariae <zakariae>
2022-05-10 19:47:01 +0200

    5000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 const { imageUploader } = require("./middlewares/imageUploader");
 const cors = require("cors");
 const mongoose = require("mongoose");
